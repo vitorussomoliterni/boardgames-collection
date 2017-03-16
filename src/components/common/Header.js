50988ad20ca3dc88ed373c8e904ce1ce8c9aff19
Vito Russo Moliterni <vito.russo.moliterni@gmail.com>
2017-03-16 16:54:21 +1100

    Playing with the nav bar

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -4,10 +4,10 @@ import {Link} from 'react-router-dom';
 class Header extends React.Component {
     render() {
         return(
-            <nav className="navbar navbar-default">
+            <nav className="navbar navbar-inverse bg-inverse">
                 <ul className="nav navbar-nav">
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/about">About</Link></li>
+                    <li className="nav-item"><Link className="nav-link"to="/">Home</Link></li>
+                    <li className="nav-item"><Link className="nav-link"to="/about">About</Link></li>
                 </ul>
             </nav>
         );
