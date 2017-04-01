212ece676cc29fc84dbee630bd0ce563e05802d1
Vito Russo Moliterni <vito.russo.moliterni@gmail.com>
2017-04-01 22:45:05 +1100

    Added logo and favicon

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     return(
         <nav className="navbar navbar-toggleable-sm navbar-inverse bg-inverse">
             <div className="navbar-nav collapse navbar-collapse" id="header-nav">
-                <Link className="navbar-brand" to="/">Home</Link>
+                <Link className="navbar-brand" to="/"><img src="img/logo.png" width="30" height="30" className="d-inline-block align-top" alt="" /></Link>
                 <Link className="nav-item nav-link" to="/games">Games</Link>
                 <Link className="nav-item nav-link" to="/about">About</Link>
             </div>
