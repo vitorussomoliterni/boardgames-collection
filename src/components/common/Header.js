a79f26695eb32e8a0a99b050ad5ea29747086c1a
Vito Russo Moliterni <vito.russo.moliterni@gmail.com>
2017-04-01 22:21:14 +1100

    Made navbar look better on mobile

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -4,9 +4,6 @@ import {Link} from 'react-router-dom';
 const Header = () => {
     return(
         <nav className="navbar navbar-toggleable-sm navbar-inverse bg-inverse">
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#header-nav" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
             <div className="navbar-nav collapse navbar-collapse" id="header-nav">
                 <Link className="navbar-brand" to="/">Home</Link>
                 <Link className="nav-item nav-link" to="/games">Games</Link>
