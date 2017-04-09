7239e8c7b33e1785ea95a1035b2b8d5f133abc4b
Vito Russo Moliterni <vito.russo.moliterni@gmail.com>
2017-04-09 18:03:43 +1000

    Added list component

--- src/components/boardgames/BoardgamesPage.js
+++ src/components/boardgames/BoardgamesPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import BoardgamesList from './BoardgamesList';
 
 class BoardgamesPage extends React.Component {
 	render() {
 		return (
 			<div>
 				<h1>Boardgames</h1>
+				<BoardgamesList />
 			</div>
 		);
 	}
