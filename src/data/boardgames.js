ea9bc33be05b3726d71fe4dda25d2e2246a87c49
Vito Russo Moliterni <vito.russo.moliterni@gmail.com>
2017-04-09 18:03:08 +1000

    Added hardcoded data

---
+++ src/data/boardgames.js
@@ -0,0 +1,22 @@
+const boardgames = [
+  {
+    id: "twilight-struggle",
+    title: "Twilight Struggle",
+    numberOfPlayers: "2",
+    score: 5
+  },
+  {
+    id: "terra-mystica",
+    title: "Terra Mystica",
+    numberOfPlayers: "2-5",
+    score: 5
+  },
+  {
+    id: "star-realms",
+    title: "Star Realms",
+    numberOfPlayers: "2",
+    score: 4
+  }
+];
+
+export default boardgames;
