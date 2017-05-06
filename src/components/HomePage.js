b9897e19b5222c41d5de33572f61682380be5f8e
Vito Russo Moliterni <vito.russo.moliterni@gmail.com>
2017-05-07 08:54:07 +1000

    Added button to home page, changed wording

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class HomePage extends React.Component {
-    render() {
-        return(
-            <div className="jumbotron">
-                <h1>Manage your boardgame collection with React!</h1>
-            </div>
-        );
-    }
+  render() {
+    return(
+      <div className="jumbotron">
+        <div className="text-center">
+          <h1>Manage your boardgame collection</h1>
+          <Link to="/boardgames"
+            className="btn btn-lg btn-primary">Let's Game!</Link>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default HomePage;
